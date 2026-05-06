47c83d70f013272ce8893fbd071c9ce111731d03
Bhanumathi0502 <bbhanuprasad82@gmail.com>
2026-05-06 22:42:42 +0530

    Uploaded by Bhanumathi

---
+++ arrowfunction.js
@@ -0,0 +1,16 @@
+
+
+let tip = amount => {
+    return amount+(amount*.2);
+}
+//  33*.2 ==6.6
+
+console.log(tip(11));
+console.log(tip(33));
+
+
+console.log(tip(121));
+
+
+
+// 121*.2=24.2+121=145.2
